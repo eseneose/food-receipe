7f135025574e6d0c350c0c56ec05772602124468
eseneose <eseneose96@gmail.com>
2022-03-19 23:48:35 +0100

    Updates

---
+++ precache-manifest.b7270140927ec14d420ad41f12643a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55b4f92b057a187c7c9c524737785a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a3f486ef9a1fd53b93",
+    "url": "/static/css/main.5471e067.chunk.css"
+  },
+  {
+    "revision": "d3931f2cdbcde24dc84f",
+    "url": "/static/js/2.91134756.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91134756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a3f486ef9a1fd53b93",
+    "url": "/static/js/main.5acc3c5f.chunk.js"
+  },
+  {
+    "revision": "cde4f86d380bf81751b1",
+    "url": "/static/js/runtime-main.5000fe9e.js"
+  }
+]);
